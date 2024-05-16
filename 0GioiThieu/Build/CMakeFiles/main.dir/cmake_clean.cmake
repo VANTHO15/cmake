8a52365376d81a047bf705fdc85cecb6c685392a
Nguyễn Văn Thọ <56969447+VANTHO15@users.noreply.github.com>
2024-05-16 16:48:31 +0700

    Add files via upload

---
+++ 0GioiThieu/Build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.c.obj"
+  "CMakeFiles/main.dir/main.c.obj.d"
+  "main"
+  "main.manifest"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 0GioiThieu/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+	printf("Hello ThoNV");
+    sum(1,2);
+	return 1;
+}
